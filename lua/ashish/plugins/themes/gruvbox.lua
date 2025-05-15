f6be68d378a255f1b6f4e8f5559ea24f5a112303
ashish2508 <jha250805@gmail.com>
2025-05-15 20:25:35 +0530

    theme rice

--- lua/ashish/plugins/themes/gruvbox.lua
+++ lua/ashish/plugins/themes/gruvbox.lua
@@ -1,32 +1,42 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1200,
-  config = function()
-    -- Default options:
-    require("gruvbox").setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = true,
-      invert_signs = true,
-      invert_tabline = true,
-      invert_intend_guides = true,
-      inverse = true,    -- invert background for search, diffs, statuslines and errors
-      contrast = "soft", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = true,
-      transparent_mode = true,
-    })
-    --vim.cmd("colorscheme gruvbox")
-  end,
+  {
+    "ricardoraposo/gruvbox-minor.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      
+      
+      -- Make it transparent
+      vim.api.nvim_command("highlight Normal guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight NormalNC guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight NormalFloat guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight SignColumn guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight LineNr guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight CursorLineNr guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight FloatBorder guibg=NONE ctermbg=NONE")
+      
+      -- Make statusline and lualine transparent
+      vim.api.nvim_command("highlight StatusLine guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight StatusLineNC guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight StatusLineTerm guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight StatusLineTermNC guibg=NONE ctermbg=NONE")
+      
+    
+      -- Make tabline transparent as well
+      vim.api.nvim_command("highlight TabLine guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight TabLineFill guibg=NONE ctermbg=NONE")
+      vim.api.nvim_command("highlight TabLineSel guibg=NONE ctermbg=NONE")
+      
+      -- Fix colors for inactive statusline sections (if needed)
+      vim.api.nvim_command("highlight link lualine_a_inactive lualine_a_normal")
+      vim.api.nvim_command("highlight link lualine_b_inactive lualine_b_normal")
+      vim.api.nvim_command("highlight link lualine_c_inactive lualine_c_normal")
+
+
+      -- Set the colorscheme
+      -- vim.cmd([[colorscheme gruvbox-minor]]) -- for oh-lucy
+
+    end
+  }
 }
--- lua/ashish/plugins/themes/vercel.lua
+++ lua/ashish/plugins/themes/vercel.lua
@@ -5,7 +5,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("vercel-dark")
+     -- vim.cmd.colorscheme("vercel-dark")
 
       require('vercel').setup({
         transparent = true,
