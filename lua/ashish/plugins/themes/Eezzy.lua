8129947f1447a88f11cf0223fd7932b89b5b6015
ashish2508 <jha250805@gmail.com>
2025-05-20 02:05:58 +0530

    Made my very own theme

---
+++ lua/ashish/plugins/themes/Eezzy.lua
@@ -0,0 +1,26 @@
+return { 
+  'ashish2508/Eezzy.nvim',  -- GitHub repository path
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local ok, eezzy = pcall(require, 'Eezzy')
+    
+    if ok and eezzy and type(eezzy.setup) == "function" then
+      eezzy.setup({
+        transparent = false,
+        italics = {
+          comments = false,
+          keywords = true,
+          functions = true,
+          strings = false,
+          variables = true,
+        },
+        overrides = {},
+      })
+    else
+      print("Failed to load eezzy module or setup function")
+    end
+    
+    vim.cmd.colorscheme("Eezzy-dark")
+  end
+}
